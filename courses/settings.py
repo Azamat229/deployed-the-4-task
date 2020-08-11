0ccea7a868e7a8f032759ba46b8141e05b09e139
Azamat <azamatmambetaliev229@gmail.com>
2020-08-11 22:05:27 +0600

    hide the secket key

--- courses/settings.py
+++ courses/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p(#!mvi#vgj(!!_rb$q(r_hh2^ez6iwh24byczm7xzuacgygch'
-
+# SECRET_KEY = 'p(#!mvi#vgj(!!_rb$q(r_hh2^ez6iwh24byczm7xzuacgygch'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['hp-228.herokuapp.com']
 
